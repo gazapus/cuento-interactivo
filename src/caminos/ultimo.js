3182b63f9233ace1581d65f9ce1ef52e5eea6e9e
Cristian Villafane <cristianv129@hotmail.com>
2020-10-20 19:40:53 -0300

    imagenes final

--- src/caminos/ultimo.js
+++ src/caminos/ultimo.js
@@ -3,15 +3,15 @@ const ultimo = {
     content: [
         {
             text: 'Al día siguiente la Princesa ya tenía lista su presentación, había encontrado una gran herramienta que la ayudaría a realizar su tesis, a pesar de lo imprediscible que parecía logró conseguir la palta dorada y así demostrar al destino que ella llegó más lejos de lo que esperaba.',
-            img: './src/images/1.jpg'
+            img: './src/images/u1.jpg'
         },
         {
             text: 'A su vez los gazapos, estuvieron muy satisfechos con la expieriencia sabiendo que no sólo consiguieron la palta, sino también tomaron conciencia de que el amor se cultiva, se cosecha y se desayuna, el amor es el que hace brillar los días, las vidas y las paltas, y que no importa el camino cuando es firme el objetivo porque la suerte puede ser asombrosa así como la palta de sabrosa, donde distintos puntos de partida convergen en una sola cosa que aunque el resultado puede ser distinto la felicidad siempre es gazaposa.',
-            img: './src/images/1.jpg'
+            img: './src/images/u2.jpg'
         },
         {
             text: '"¡Feliz cumplemes N° 33 Princesa Cascabel le desea el Principe Gazapo y todo el elenco de Convergencia Gazaposa!',
-            img: './src/images/1.jpg'
+            img: './src/images/u3.jpg'
         }
     ],
     options: [
